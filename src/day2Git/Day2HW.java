632d6c468b7acc68e13b34f7e76747dd74e74012
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-10 14:24:34 -0700

    implemented divide() and push

--- src/day2Git/Day2HW.java
+++ src/day2Git/Day2HW.java
@@ -8,4 +8,15 @@ public class Day2HW {
 		
 		System.out.println("Remote Repository");
 	}
+	
+	//Stash
+	public void multiple(int a, int b) {
+		System.out.println(a*b);
+	}
+	
+	//Push
+	public void divide(int x, int y) {
+		System.out.println(x/y);
+	}
+	
 }
